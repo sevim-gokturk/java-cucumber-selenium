f64ca2e2e12595152ceecc55be5b91bba4620150
sevim-gokturk <sevim.gokturk@powercoders.org>
2025-01-08 10:21:06 +0100

    Add GitHub Actions workflow for running Cucumber tests/Build issue- json report-dependencies

--- src/test/java/runners/RegressionTestRunner.java
+++ src/test/java/runners/RegressionTestRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         plugin = {
                 "pretty",
                 "html:target/default-cucumber-reports.html",
-                "json:target/json-reports/cucumber.json",
+                //"json:target/json-reports/cucumber.json",
                 "junit:target/xml-report/cucumber.xml",
                 "rerun:target/failedRerun.txt",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {
                 "html:target/default-cucumber-reports.html",
-                "json:target/json-reports/cucumber.json",
+                //"json:target/json-reports/cucumber.json",
                 "junit:target/xml-report/cucumber.xml",
                 "rerun:target/failedRerun.txt",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
