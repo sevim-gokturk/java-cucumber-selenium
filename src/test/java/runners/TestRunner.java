4e1a6e9bbc24219b634a46115e67a5795c59dc5e
sevim-gokturk <sevim.gokturk@powercoders.org>
2025-01-09 08:14:39 +0100

    deleting testNG dependencies

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         },
         features = "./src/test/resources/features",
         glue = {"stepdefinitions","hooks"},//path of the step definitions folder
-        tags = "@excel",
+        tags = "@smoke",
         dryRun = false
 )
 public class TestRunner {
